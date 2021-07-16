0ad96f2f23180907565dc841654114ed5349b54a
Anas MASSNAOUI <anasmassnaoui@gmail.com>
2021-07-16 20:16:57 +0100

    feat: implement new charts, extend statistics on backend

--- pages/api/statistics.ts
+++ pages/api/statistics.ts
@@ -1,28 +1,85 @@
 import { parseFile } from 'fast-csv'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+const Student_ID_Column = 0
+const Semster_Name_Column = 1
+const Paper_ID_Column = 2
+const Paper_Name_Column = 3
+const Marks_Column = 4
 
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
 
-    let statistics: any = {}
+    let _statistics: any = {
+        semsters: {},
+        marks: {}
+    }
     let header: any
-
     parseFile("./public/uploads/dataset.csv")
-    .on('error', error => res.status(200).json({ error }))
+    .on('error', error => {
+        console.log(error)
+        res.status(200).json({ error })
+    })
     .on('data', row => {
-        if (!header) {
-            header = row
-            header.forEach((column: string) => statistics[column] = { len: 0, keys: { }});
-        }
+        if (!header) header = row
         else {
-            row.forEach((data: string, index: number) => {
-                const column = header[index]
-                statistics[column].len += 1
-                statistics[column].keys[data] = statistics[column].keys[data] ? statistics[column].keys[data] + 1 : 1
-            });
+            const Student_ID = row[Student_ID_Column]
+            const Semster_Name = row[Semster_Name_Column]
+            const Paper_ID = row[Paper_ID_Column]
+            const Paper_Name = row[Paper_Name_Column]
+            //const Marks = Math.floor(Math.random() * 100)
+            const Marks = parseInt(row[Marks_Column])
+
+            if (!_statistics.semsters[Semster_Name])
+                _statistics.semsters[Semster_Name] = { analyse: {}, marks: {} }
+            
+                
+            const { notes=0, count=0, min=100, max=0 } = _statistics.semsters[Semster_Name].analyse 
+            
+            _statistics.semsters[Semster_Name].analyse = {
+                notes: notes + Marks,
+                count: count + 1,
+                min: min > Marks ? Marks : min,
+                max: max < Marks ? Marks : max
+            }
+            if (!_statistics.semsters[Semster_Name].marks[Marks])
+                _statistics.semsters[Semster_Name].marks[Marks] = 0
+            if (!_statistics.marks[Marks])
+                _statistics.marks[Marks] = 0
+            _statistics.semsters[Semster_Name].marks[Marks] += 1
+            _statistics.marks[Marks] += 1
         }
     })
-    .on('end', (rowCount: number) => res.status(200).json(statistics));
+    .on('end', (rowCount: number) => {
+        let statistics: any = {
+            Semsters: {
+                analyse: [],
+                marks: {}
+            }
+        }
+        Object.keys(_statistics.semsters).forEach((key, index) => {
+            const { analyse } = statistics['Semsters']
+            const { notes, count, min, max } = _statistics.semsters[key].analyse
+            statistics['Semsters'].analyse = [...analyse, {
+                name: key,
+                moyenne: notes / count,
+                min,
+                max
+            }]
+            statistics['Semsters'].marks[key] = Object.keys(_statistics.semsters[key].marks).map(_key => ({
+                mark: parseInt(_key),
+                count: _statistics.semsters[key].marks[_key],
+                index: index + 1,
+            }))
+
+        })
+
+        statistics.marks = Object.keys(_statistics.marks).map(key => ({
+            name: parseInt(key),
+            count: _statistics.marks[key]
+        }))
+
+        res.status(200).json(statistics)
+    });
 
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,11 @@ import styles from '../styles/Home.module.css'
 import 'antd/dist/antd.css';
 import { InboxOutlined } from '@ant-design/icons';
 import { UploadChangeParam, UploadFile } from 'antd/lib/upload/interface';
-import { message } from 'antd';
+import { Col, message, Row } from 'antd';
 import { useEffect, useState } from 'react';
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
+import { BarChart, ScatterChart, Scatter, Bar, Tooltip, Legend, XAxis, YAxis, ZAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 import axios from 'axios'
 import { Spin } from 'antd';
-
-const colors = ['#00429d', '#2e59a8', '#4771b2', '#5d8abd', '#73a2c6', '#8abccf', '#a5d5d8', '#c5eddf', '#ffffe0']
-
 
 const Upload = ({ onChange=(info: UploadChangeParam<UploadFile<any>>) => {} }) => {
 
@@ -25,72 +22,122 @@ const Upload = ({ onChange=(info: UploadChangeParam<UploadFile<any>>) => {} }) =
     </p>
     <p className="ant-upload-text">Click or drag file to this area to upload</p>
     <p className="ant-upload-hint">
-      please imprt csv file that include the following fields
+      please import a csv file that include the following fields
       <br />
       [ 'Student_ID', 'Semster_Name', 'Paper_ID', 'Paper_Name', 'Marks' ]
     </p>
   </Dragger>)
 }
 
-const Statistics = () => {
-  const [ isLoading, setIsLoading ] = useState(true)
-  const [ data, setData ] = useState([])
+const BarStatistics = ({ data } : { data: any }) => {
 
-  useEffect(() =>{
-    axios.get("/api/statistics").then(res =>{
-      const statistics = res.data
-      let data: any = []
-      Object.keys(statistics['Marks'].keys).forEach(key => {
-        data.push({
-          name: key,
-          count: statistics['Marks'].keys[key]
-        })
-      });
-      setData(data)
-      setIsLoading(false)
-    })
-  }, [])
+  return (
+    <ResponsiveContainer className={styles.chart} height={300}>
+      <BarChart
+        width={500}
+        height={300}
+        data={data}
+        margin={{
+          top: 20,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis yAxisId="left" orientation="left" stroke="#8884d8" />
+        <YAxis yAxisId="center" orientation="right" stroke="#82ca9d" />
+        <YAxis yAxisId="right" orientation="right" stroke="#ca8187" />
+        <Tooltip />
+        <Legend />
+        <Bar yAxisId="left" dataKey="min" fill="#8884d8" />
+        <Bar yAxisId="center" dataKey="moyenne" fill="#82ca9d" />
+        <Bar yAxisId="right" dataKey="max" fill="#ca8187" />
+      </BarChart> 
+  
+  </ResponsiveContainer>)
+}
 
-  const getPath = (x: number, y: number, width: number, height: number) => `M${x},${y + height}
-          C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3} ${x + width / 2}, ${y}
-          C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
-          Z`;
 
-  const TriangleBar = ({x=0, y=0, width=0, height=0, fill='red'} = {}) => {
-    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-  };
+const ScatterStatistics = ({ data } : { data: any }) => {
+  
+  return (
+    <ResponsiveContainer className={styles.chart} height={300}>
+        <ScatterChart
+        width={400}
+        height={400}
+        margin={{
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20,
+        }}
+      >
+        <CartesianGrid />
+        <XAxis type="number" dataKey="name" name="note" unit="" />
+        <YAxis type="number" dataKey="count" name="count" unit="" />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Scatter name="A school" data={data} fill="#8884d8" />
+      </ScatterChart>
+      </ResponsiveContainer>
+    )
+}
 
-  return (<ResponsiveContainer className={styles.chart}>
-            { !isLoading ?
-            <BarChart
-              width={500}
-              height={300}
-              data={data}
+
+const ScatterTimeLineStatistics = ({ data } : { data : any }) => {
+
+  return (
+    <div className={styles.chart} style={{ minHeight: 200 }}>
+      {Object.keys(data).map((key: string, index: number, keys: Array<string>) => (
+        <ResponsiveContainer width="100%" height={60} key={key}>
+            <ScatterChart
+              width={800}
+              height={60}
               margin={{
-                top: 20,
-                right: 30,
-                left: 20,
-                bottom: 5,
+                top: 10,
+                right: 0,
+                bottom: 0,
+                left: 0,
               }}
             >
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="name" />
-              <YAxis />
-              <Bar dataKey="count" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-                {data.map((entry, index) => (
-                  <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-                ))}
-              </Bar>
-            </BarChart> : 
-            <Spin size="large" />
-            }
-          </ResponsiveContainer>)
+              <XAxis
+                type="category"
+                dataKey="mark"
+                name="mark"
+                interval={0}
+                tick={ keys.length - 1 != index ? { fontSize: 0 } : {} }
+                tickLine={{ transform: 'translate(0, -6)' }}
+              />
+              <YAxis
+                type="number"
+                dataKey="index"
+                name="semster"
+                height={10}
+                width={80}
+                tick={false}
+                tickLine={false}
+                axisLine={false}
+                label={{ value: key, position: 'insideRight' }}
+              />
+              <ZAxis type="number" dataKey="count" range={[16, 200]} domain={[0, 100]} />
+              <Tooltip cursor={{ strokeDasharray: '3 3' }} wrapperStyle={{ zIndex: 100 }}  />
+              <Scatter data={data[key]} fill="#8884d8" />
+            </ScatterChart>
+          </ResponsiveContainer>
+      ))
+    }
+    </div>
+  )
 
 }
+
 
 export default function Home() {
 
   const [ isUploading, setIsUploading ] = useState(true)
+  const [ isLoading, setIsLoading ] = useState(true)
+  const [ statistics, setStatistics ] = useState<any>({})
 
   const onChange = (info: UploadChangeParam<UploadFile<any>>) => {
     const { status } = info.file
@@ -101,6 +148,13 @@ export default function Home() {
     if (status === 'done') {
       message.success(`${info.file.name} file uploaded successfully.`);
       setIsUploading(false)
+
+      axios.get("/api/statistics").then(res =>{
+        const statistics = res.data
+        setStatistics(statistics)
+        setIsLoading(false)
+      })
+      
     } else if (status === 'error') {
       message.error(`${info.file.name} file upload failed.`);
     }
@@ -109,7 +163,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>PFE Statistics</title>
         <meta name="description" content="Generated by create next app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
@@ -120,11 +174,21 @@ export default function Home() {
         </h1>
       { isUploading ?
           <Upload onChange={onChange} /> :
-          <Statistics />
+          !isLoading ? <Row style={{ width: '100%' }}>
+            <Col xs={12}>
+              <BarStatistics data={statistics.Semsters.analyse} />
+            </Col>
+            <Col xs={12}>
+              <ScatterStatistics data={statistics.marks} />
+            </Col>
+            <Col xs={24}>
+              <ScatterTimeLineStatistics data={statistics.Semsters.marks} />
+            </Col>
+          </Row> : <Spin size="large" />
       }
       </main>
       <footer className={styles.footer}>
-          Hola
+          Made By Anas & Othman
       </footer>
     </div>
   )
